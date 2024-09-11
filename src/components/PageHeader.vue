275da72bfa8979d988535cb16e03102f6fb519ab
NJ <a85124236@gmail.com>
2024-09-11 16:07:18 +0800

    add header & clear default staff

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="header">
+  <div class="logo"></div>
+  <a href="#" class="link">ABOUT</a>
+  <a href="#" class="link">NEWS</a>
+  <a href="#" class="link">ITEMS</a>
+</div>
+</template>
+
+<style scoped>
+.header{
+  position: fixed;
+  top:0;
+  width: 100%;
+  height: 80px;
+  background-color: darkgreen;
+}
+</style>
