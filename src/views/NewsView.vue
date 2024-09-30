2c6c8c2bc2bc3168f6c6301306f3c777c5842f93
NJ <a85124236@gmail.com>
2024-09-30 15:33:06 +0800

    add news&product views and routers

---
+++ src/views/NewsView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="News">
+    <h1>HELLO this is our news</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="Products">
+    <h1>HELLO here will be products</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
